80a370219cb9b53a761e15e0db2b069caec85b72
Hassan-154 <83923230+Hassan-154@users.noreply.github.com>
2025-06-14 09:58:34 +0500

    update

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 
 function Home() {
     return (
-        <div className='lg:h-[80vh] pb-[40px] lg:pb-0 max-w-6xl mx-auto flex flex-col lg:flex-row lg:gap-[10px] items-center justify-center px-3 sm:px-4 xl:px-0'>
+        <div className='lg:h-[80vh] pb-[15px] lg:pb-0 max-w-6xl mx-auto flex flex-col lg:flex-row lg:gap-[10px] items-center justify-center px-3 sm:px-4 xl:px-0'>
             <div data-aos="fade-right" className='w-2/2 lg:w-1/2 flex flex-col gap-[20px]'>
                 <h1 className='text-[42px] leading-[50px] sm:text-[60px] sm:leading-[67px] text-black font-medium'>Connecting <br></br> <span className='text-periwinkle-blue font-roslindale italic'>Finance</span> Talent with Hiring Power</h1>
                 <p className='max-w-[440px]'>A recruitment platform built for accounting & finance professionals and the recruiters who need them.</p>
@@ -19,7 +19,7 @@ function Home() {
                 </div>
             </div>
             <div data-aos="fade-left" className='w-2/2 lg:w-1/2 p-4'>
-                <div className="relative h-[320px] sm:h-[600px] w-auto overflow-hidden">
+                <div className="relative h-[360px] sm:h-[600px] w-auto overflow-hidden">
                     <Image src="/images/home-back.svg" alt='image' height="300" width="300" className="absolute top-0 left-1/2 transform -translate-x-1/2 w-auto sm:h-[560px] object-cover" />
                     <div className="absolute inset-0 flex items-center justify-center">
                         <Image src="/images/Frame 1707479238 (4).png" alt='image' height="700" width="700" className="min-w-[350px] sm:min-w-[570px] h-auto" />
