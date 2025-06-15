c3c18153e6f3a3e5e932d58eb482b66c4f414435
Hassan-154 <83923230+Hassan-154@users.noreply.github.com>
2025-06-16 01:53:21 +0500

    update

--- app/components/Button.jsx
+++ app/components/Button.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 
-function Button({ className, title }) {
+function Button({ className, title, onClick }) {
     return (
-        <button className={`px-5 py-2.5 text-sm font-thin cursor-pointer transition-colors text-center ${className}`}>
+        <button 
+            className={`px-5 py-2.5 text-sm font-thin cursor-pointer transition-colors text-center ${className}`}
+            onClick={onClick}
+        >
             {title}
         </button>
     )
